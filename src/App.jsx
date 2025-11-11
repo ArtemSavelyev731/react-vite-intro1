4069457285bf2000a721045fde29a538db0c0065
artem <121548842+ArtemSavelyev731@users.noreply.github.com>
2025-11-11 19:50:04 +0300

    +3 hours

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,19 @@
 import Header from "./components/Header";
-import WayToTeach from "./components/WayToTeach";
-import Button from "./components/Button/Button";
-import { useState } from "react";
-import { ways } from "./data";
+import TeachingSection from "./components/TeachingSection";
+import DifferencesSection from "./components/DifferencesSection";
+import { Fragment } from 'react'
+import IntroSection from "./components/IntroSection";
 
-function App() {
-  const [content, setContent] = useState("нажми на кнопку");
-
-  // let content = 'нажми на кнопку'
-
-  const handleClick = (type) => {
-    // console.log('Button clicked', type);
-    setContent(type);
-    //content = type
-  };
-
+export default function App() {
   return (
-    <div>
+    <>
       <Header />
       <main>
-        <section>
-          <h3>наш подход к обучению test</h3>
-          <ul>
-            <WayToTeach
-              title={ways[0].title}
-              description={ways[0].description}
-            />
-            <WayToTeach {...ways[1]} />
-            <WayToTeach {...ways[2]} />
-          </ul>
-        </section>
-        <section>
-          <h3>Чем мы отличаемся от других</h3>
-          <Button onClick={() => handleClick("way")}>Подход</Button>
-          <Button onClick={() => handleClick("easy")}>Доступность</Button>
-          <Button onClick={() => handleClick("program")}>Концентрация</Button>
-
-          <p>{content}</p>
-        </section>
+        <IntroSection /> 
+        <TeachingSection />
+        <DifferencesSection />
       </main>
-    </div>
-  );
+    </>
+  )
 }
 
-export default App;
